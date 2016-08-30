a9e3db7f1ca948b2a50e4a996981ea294ce8d79a
zmcdaniel <zmcdaniel@gmail.com>
2016-08-30 14:24:12 -0700

    Passed all tests

--- lib/mailbox.rb
+++ lib/mailbox.rb
@@ -1,13 +1,22 @@
 class MailBox
+
+  attr_accessor :letters, :packages
+
   def initialize(letters, packages)
+    @letters = letters
+    @packages = packages
   end
 
   def add_letter
+    @letters = letters + 1
   end
 
   def add_package
+    @packages = packages + 1
   end
 
   def clear
+    @packages = 0
+    @letters = 0
   end
 end
--- lib/tree.rb
+++ lib/tree.rb
@@ -1,7 +1,14 @@
 class Tree
+
+  attr_accessor :type, :height, :age
+
   def initialize(type, height, age)
+    @type = type
+    @height = height
+    @age = age
   end
 
   def grow_tree(water_amount)
+    @height = height + (water_amount * 0.1)
   end
 end
